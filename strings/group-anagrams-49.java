1162efe5f26b00adbde09cee9ea3c334c7dcc49f
Prakhar Katiyar <8090353771s@gmail.com>
2026-02-01 00:01:46 +0530

    Add Group Anagrams solution

---
+++ strings/group-anagrams-49.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String,List<String>> map = new HashMap<>();
+        for(int i = 0; i < strs.length; i++){
+            char [] ch = strs[i].toCharArray();
+            Arrays.sort(ch);
+            String Key = new String (ch);
+            if(map.containsKey(Key)){
+                map.get(Key).add(strs[i]);
+            }
+            else{
+                List<String> ls = new ArrayList<>();
+                ls.add(strs[i]);
+                map.put(Key, ls);
+            }
+        }
+        return new ArrayList<>(map.values());
+    }
+}
